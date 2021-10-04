ccf53e177f4201c67db8d2531d0a573405a1736e
tkhs <spain3405@gmail.com>
2021-10-05 07:51:06 +0900

    adjust blog

--- src/layouts/post.js
+++ src/layouts/post.js
@@ -2,6 +2,7 @@ import React from 'react';
 import _ from 'lodash';
 import moment from 'moment-strftime';
 import Prism from 'prismjs';
+import Router from 'next/router';
 
 import { Layout } from '../components/index';
 import { htmlToReact, withPrefix, markdownify, Link } from '../utils';
@@ -17,14 +18,14 @@ export default class Post extends React.Component {
     const data = _.get(this.props, 'data');
     const config = _.get(data, 'config');
     const page = _.get(this.props, 'page');
-    // const title = _.get(page, 'title');
-    // const subtitle = _.get(page, 'subtitle');
-    // const image = _.get(page, 'image');
-    // const imageAlt = _.get(page, 'image_alt', '');
     const date = _.get(page, 'date');
     const dateTimeAttr = moment(date).strftime('%Y-%m-%d %H:%M');
     const formattedDate = moment(date).strftime('%A, %B %e, %Y');
-    // const markdownContent = _.get(page, 'markdown_content');
+
+    const onClickBack = (e) => {
+      e.preventDefault();
+      Router.back();
+    }
 
     return (
       <Layout page={page} config={config}>
@@ -32,17 +33,14 @@ export default class Post extends React.Component {
           <article className="post post-full">
             <header className="post-header inner-sm">
               <h1 className="post-title line-top">{post.title}</h1>
-              {/* {subtitle && <div className="post-subtitle">{htmlToReact(subtitle)}</div>} */}
+              <div className="post-subtitle">
+                <span>{post.category.map(cat => <label key={cat}>{cat}</label>)}</span>
+                <span className="post-date">{post.date}</span>
+              </div>
             </header>
-            {/* {image && (
-                            <div className="post-image">
-                                <img src={withPrefix(image)} alt={imageAlt} />
-                            </div>
-                        )} */}
-            {/* {markdownContent && <div className="post-content inner-sm">{markdownify(markdownContent)}</div>} */}
             {content.map(item => (
               <div className="post-content inner-sm" key={item.block.id}>
-{item.type === 'code' && console.log(item.block.code.language)}
+  {/* {item.type === 'code' && console.log(item.block.code.language)} */}
                 {(item.type === 'heading_2') ? <p className="heading-2">{item.block.heading_2.text[0].text.content}</p> : ''}
                 {(item.type === 'heading_3') ? <p className="heading-3">{item.block.heading_3.text[0].text.content}</p> : ''}
                 {(item.type === 'image') ? <div className="capture-wrap"><img src={item.block.image.file.url} className="capture-image" /></div> : ''}
@@ -51,8 +49,8 @@ export default class Post extends React.Component {
                   <div>
                     {(item.block.paragraph.text.length > 0) ? (
                       item.block.paragraph.text.map(text => (text.href) ? (
-                        <Link href={text.href} target="_blank" key={text.href}>{text.plain_text}</Link>
-                      ) : <span key={text.href}>{text.plain_text}</span>)
+                        <Link href={text.href} target="_blank" key={text.id}>{text.plain_text}</Link>
+                      ) : <span key={text.id}>{text.plain_text}</span>)
                     ) : "　"}
                   </div>
                 ) : ''}
@@ -63,8 +61,8 @@ export default class Post extends React.Component {
                 ) : ''}
               </div>
             ))}
-            <footer className="post-meta inner-sm">
-              <time className="published" dateTime={dateTimeAttr}>{formattedDate}</time>
+            <footer className="post-meta inner-sm back-btn">
+              <button className="button" onClick={onClickBack}>戻る</button>
             </footer>
           </article>
         </div>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import '../sass/main.scss';
-import "prismjs/themes/prism.css";
+import "prismjs/themes/prism-vsc-dark.css";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
