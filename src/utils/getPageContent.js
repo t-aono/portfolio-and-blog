6fbb255b4b566eae0a5e8958ca18ed044965a91a
tkhs <spain3405@gmail.com>
2021-12-28 08:58:45 +0900

    fix css

--- src/utils/getPageContent.js
+++ src/utils/getPageContent.js
@@ -2,7 +2,7 @@ const { Client } = require('@notionhq/client');
 
 export default async function getPageContent(pageId) {
   const notion = new Client({ auth: process.env.NOTION_TOKEN });
-  const children = await notion.blocks.children.list({ block_id: pageId, page_size: 100 });
+  const children = await notion.blocks.children.list({ block_id: pageId, page_size: 100 }); // max 100
   let content = [];
   children.results.map((block) => {
     content.push({ type: block.type, block: block });
