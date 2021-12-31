9b9e91eb610f31a11811f19f0739012b9a4fe945
t-aono <46856574+t-aono@users.noreply.github.com>
2021-12-31 20:15:45 +0900

    Feature/#11 access analysis tool (#20)
    * set ga
    
    * reset build setting

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,23 @@
 import '../sass/main.scss';
 import 'prism-themes/themes/prism-vsc-dark-plus.css';
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+import * as gtag from '../lib/gtag';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
-// This default export is required in a new `pages/_app.js` file.
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
+
+export default MyApp;
