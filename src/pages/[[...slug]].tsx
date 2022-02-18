2ecb7174995c5e4775260187ffb5c261335a9ee5
aono <info@t-aono.net>
2022-02-18 20:24:30 +0900

    make top page

--- src/pages/[[...slug]].tsx
+++ src/pages/[[...slug]].tsx
@@ -8,21 +8,45 @@ import matter from 'gray-matter';
 import { getProjects, getPosts, getCategories, makePostCollection, makeProjectCollection, makeCategoryList } from '../utils';
 import pageLayouts from '../layouts';
 
-const getPage = (page) => {
-  const pageMdPath = path.join(process.cwd(), 'content', 'pages', `${page}.md`);
-  // console.log(pageMdPath);
-  const pageMd = fs.readFileSync(pageMdPath, 'utf-8');
-  console.log(matter(pageMd));
-  // return matter(pageMd);
-  // return {
-  //   page: matter(pageMd)
-  // };
+const getMarkdownData = (pageUrl) => {
+  let markdownFile = '';
+  switch (pageUrl) {
+    case '/':
+      markdownFile = 'index.md';
+      break;
+    case '/portfolio':
+      markdownFile = 'portfolio.md';
+      break;
+    case '/blog':
+      markdownFile = 'blog.md';
+      break;
+    case '/contact':
+      markdownFile = 'contact.md';
+      break;
+  }
+  const filePath = path.join(process.cwd(), 'content', 'pages', markdownFile);
+  const pageMd = fs.readFileSync(filePath, 'utf-8');
+  const { data } = matter(pageMd);
+  return data;
 };
 
 const getConfig = () => {
   const configJsonPath = path.join(process.cwd(), 'content', 'data', 'config.json');
   const configJson = fs.readFileSync(configJsonPath, 'utf-8');
   return JSON.parse(configJson);
+};
+
+const getModelName = (pagePath) => {
+  switch (pagePath) {
+    case '/':
+      return 'advanced';
+    case '/portfolio':
+      return 'portfolio';
+    case '/blog':
+      return 'blog';
+    default:
+      return 'page';
+  }
 };
 
 const Page = (props) => {
@@ -36,7 +60,8 @@ const Page = (props) => {
 
 export const getStaticPaths: GetStaticPaths = async () => {
   console.log('Page [...slug].js getStaticPaths');
-  const paths = ['/']; //await sourcebitDataClient.getStaticPaths();
+  const paths = ['/', '/portfolio', '/blog', '/contact'];
+  //await sourcebitDataClient.getStaticPaths();
   // const postPaths = (await getPosts('path')) as string[];
   // const blogPagingPaths = postPaths.map((_, i) => (i % 12 === 0 ? `/blog/paginate/${i / 12 + 2}` : null)).filter((v) => v);
   // paths.push(...blogPagingPaths);
@@ -46,14 +71,15 @@ export const getStaticPaths: GetStaticPaths = async () => {
 export const getStaticProps: GetStaticProps<{ params: ParsedUrlQuery }> = async ({ params }) => {
   console.log('Page [...slug].js getStaticProps, params: ', params);
   const pagePath = '/' + (params.slug ? (params.slug as string[]).join('/') : '');
-  const page = getPage('index');
   const props = {
-    page: { __metadata: { modelName: 'advanced' }, ...page },
+    page: {
+      __metadata: { modelName: getModelName(pagePath), urlPath: pagePath },
+      ...getMarkdownData(pagePath)
+    },
     data: {
       config: getConfig()
     }
   };
-  //await sourcebitDataClient.getStaticPropsForPageAtPath(pagePath);
 
   if (params.slug && params.slug[0] === 'portfolio') {
     const responseProjects = await getProjects();
@@ -90,5 +116,4 @@ export const getStaticProps: GetStaticProps<{ params: ParsedUrlQuery }> = async 
   return { props };
 };
 
-// export default withRemoteDataUpdates(Page);
 export default Page;
